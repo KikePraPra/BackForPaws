d718e2ba828a11caa27d599df84092da03b40565
KikePraPra <josezumbado02@gmail.com>
2025-06-26 13:42:00 -0600

    Proyecto inicializado

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    //
+}
+
+// php artisan make:controller NombreController --resource
+// php artisan make:migration create_nombreS_table (el nombre debe contener S al final)
+// php artisan migrate
+// php artisan make:model nombre (no lleva S)
