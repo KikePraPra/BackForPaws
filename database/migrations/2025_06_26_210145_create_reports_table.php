e70cf3ff07005ed80d309fec3290f4a34e5557cd
KikePraPra <josezumbado02@gmail.com>
2025-06-26 15:15:51 -0600

    Se creó el primer controlador y las primeras migraciones, aún en vista de ser revisadas

---
+++ database/migrations/2025_06_26_210145_create_reports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')->constrained('accounts')->onDelete('cascade');
+            $table->foreignId('animal_id')->constrained('animals')->onDelete('cascade');
+            $table->string('pet_name');
+            $table->integer('pet_age');
+            $table->string('last_place');
+            $table->string('pet_image');
+            $table->text('description');
+            $table->string('pet_state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
